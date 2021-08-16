912df3be291d5154d29229bc172db5fc9bb6533d
Hex <balovijayagan@gmail.com>
2021-08-16 09:55:16 +0000

    oh yeah

--- add-new-remote.sh
+++ add-new-remote.sh
@@ -6,6 +6,6 @@ read -p "Destination folder name:" ZZZ
 
 git remote add $gitremotename $gitremotelink
 git fetch gitremotename
-git merge -s ours --no-commit --allow-unrelated-histories gitremotename/master
-git read-tree --prefix=$ZZZ/ -u gitremotename/master
+git merge -s ours --no-commit --allow-unrelated-histories $gitremotename/master
+git read-tree --prefix=$ZZZ/ -u $gitremotename/master
 git commit -m "Imported this as a subtree."
