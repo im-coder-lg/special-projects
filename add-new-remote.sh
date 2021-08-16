10dd67394e1a1492c10d98b5fcecb7820d6c8ba9
Hex <balovijayagan@gmail.com>
2021-08-16 09:57:56 +0000

    ARGGGG but not anymore

--- add-new-remote.sh
+++ add-new-remote.sh
@@ -4,8 +4,4 @@ read -p "Git remote link(use git link only): " gitremotelink
 read -p "Git Remote name: " gitremotename
 read -p "Destination folder name:" ZZZ
 
-git remote add $gitremotename $gitremotelink
-git fetch gitremotename
-git merge -s ours --no-commit --allow-unrelated-histories $gitremotename/master
-git read-tree --prefix=$ZZZ/ -u $gitremotename/master
-git commit -m "Imported this as a subtree."
+git remote add $gitremotename $gitremotelink && git fetch $gitremotename && git merge -s ours --no-commit --allow-unrelated-histories $gitremotename/master && git read-tree --prefix=$ZZZ/ -u $gitremotename/master && git commit -m "Imported this as a subtree."
